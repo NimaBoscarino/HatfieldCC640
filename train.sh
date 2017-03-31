3991fb3e82267724c92ab9304060e447ab6416c4
Nima Boscarino <nima.boscarino@gmail.com>
2017-03-31 16:46:33 -0700

    Fix order of tf_files creation
    This way it doesn’t matter where you clone the repo

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 # Bunch of docker stuff
 
+mkdir $HOME/tf_files
+cp Months $HOME/tf_files/Months
+
 cd $HOME
 mkdir tf_files
 cd tf_files
-
-cp ~/HatfieldCC640/Months Months
 
 docker run -it -v $HOME/tf_files:/tf_files  gcr.io/tensorflow/tensorflow:latest-devel
 cd /tensorflow
